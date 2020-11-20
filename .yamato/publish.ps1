265094d5716534445bbc88c3919b26ab73d0a336
Francis Tremblay <francis.tremblay@unity3d.com>
2020-11-20 13:04:38 -0500

    Using LICENSE file as explicitly said in the Stevedore Arftifact Guide

--- .yamato/publish.ps1
+++ .yamato/publish.ps1
@@ -20,7 +20,7 @@ $artifacts_dir = mkdir ".\Build\Release" -Force
 $ilrepack_dir = mkdir ".\artifacts\ilrepack\" -Force
 $dest_dir = Join-Path $(Resolve-Path .\) "publish.zip"
 Copy-Item .\.yamato\README $artifacts_dir -Force
-Copy-Item .\.yamato\LICENSE $artifacts_dir -Force
+Copy-Item .\LICENSE $artifacts_dir -Force
 Remove-Item "$artifacts_dir\*.json"
 Add-Type -Assembly "System.IO.Compression.FileSystem";
 [System.IO.Compression.ZipFile]::CreateFromDirectory($artifacts_dir, $dest_dir);
@@ -31,5 +31,4 @@ $stevedore_artifactid = "$ilrepack_dir\$($ilrepack_version)_$($filehash.Hash).zi
 mv $dest_dir $stevedore_artifactid -Force
 
 Invoke-WebRequest -Uri $stevedore_upload_tool_url -OutFile ./StevedoreUpload.exe
-Write-Host ".\StevedoreUpload.exe --repo=$stevedore_repository --version=$ilrepack_version $stevedore_artifactid"
 .\StevedoreUpload.exe --repo=$stevedore_repository --version=$ilrepack_version $stevedore_artifactid
