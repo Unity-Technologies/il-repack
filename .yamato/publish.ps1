1deedf3dea2deee3bc431032f152273faa3cd121
Francis Tremblay <francis.tremblay@unity3d.com>
2020-11-26 14:26:23 -0500

    Fixed stevedore requiring a zip file name instead of a hash name

--- .yamato/publish.ps1
+++ .yamato/publish.ps1
@@ -6,8 +6,7 @@ Param (
 )
 
 # For debugging
-rmdir ".\artifacts\ilrepack\" -Force -Recurse -ErrorAction Ignore
-rm publish.zip -Force -ErrorAction Ignore
+rm ilrepack.zip -Force -ErrorAction Ignore
 rm StevedoreUpload.exe -Force -ErrorAction Ignore
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -17,8 +16,7 @@ $ProgressPreference = 'SilentlyContinue'
 # Package for stevedore
 Write-Host "Preparing publishing package..."
 $artifacts_dir = mkdir ".\Build\Release" -Force
-$ilrepack_dir = mkdir ".\artifacts\ilrepack\" -Force
-$dest_dir = Join-Path $(Resolve-Path .\) "publish.zip"
+$dest_dir = Join-Path $(Resolve-Path .\) "ilrepack.zip"
 Copy-Item ".\.yamato\README.md" $artifacts_dir -Force
 Copy-Item ".\Third party notices.md" $artifacts_dir -Force
 Copy-Item ".\LICENSE.md" $artifacts_dir -Force
@@ -26,10 +24,6 @@ Remove-Item "$artifacts_dir\*.json"
 Add-Type -Assembly "System.IO.Compression.FileSystem";
 [System.IO.Compression.ZipFile]::CreateFromDirectory($artifacts_dir, $dest_dir);
 
-# Rename with Hash
-$filehash = Get-FileHash -Algorithm SHA256 $dest_dir
-$stevedore_artifactid = "$ilrepack_dir\$($ilrepack_version)_$($filehash.Hash).zip".ToLowerInvariant()
-mv $dest_dir $stevedore_artifactid -Force
-
+Write-Host "Uploading $dest_dir($ilrepack_version) to Stevedore $stevedore_repository repositry..."
 Invoke-WebRequest -Uri $stevedore_upload_tool_url -OutFile ./StevedoreUpload.exe
-.\StevedoreUpload.exe --repo=$stevedore_repository --version=$ilrepack_version $stevedore_artifactid
+.\StevedoreUpload.exe --repo=$stevedore_repository --version=$ilrepack_version $dest_dir
